2b13d2564d9bc50cf574dcac83a3a946176841ad
hophacker <jiefeng.hopkins@gmail.com>
2019-05-13 15:46:11 +0800

    make it work with html-webpack-plugin@4.0.0-beta.5

--- src/generators/tapable.js
+++ src/generators/tapable.js
@@ -1,9 +1,9 @@
+import HtmlWebpackPlugin from '../../../html-webpack-plugin'
 import { buildResources, injectResources, generateHtmlTags, generateAppleTags, generateMaskIconLink, applyTag } from '../injector'
 
 module.exports = function (that, { hooks: { compilation: comp, emit } }) {
   comp.tap('webpack-pwa-manifest', (compilation) => {
-    if(!compilation.hooks || !compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing) return
-    compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('webpack-pwa-manifest', function (htmlPluginData, callback) {
+    HtmlWebpackPlugin.getHooks(compilation).afterTemplateExecution.tapAsync('webpack-pwa-manifest', (htmlPluginData, callback) => {
       if (!that.htmlPlugin) that.htmlPlugin = true
       buildResources(that, that.options.publicPath || compilation.options.output.publicPath, () => {
         if (that.options.inject) {
